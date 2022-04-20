6b8597b1952094a12dad985ccaadb88d96803b42
Mustafa Kerim Yilmaz <mustafakerim.yilmaz@kloeckner.com>
2022-04-20 21:39:06 +0200

    spring security

---
+++ src/main/java/com/redi/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.redi.demo.controller;
+
+import com.redi.demo.repository.UserRepository;
+import com.redi.demo.repository.model.User;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@Controller
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(path = "/user/{email}")
+    @ResponseBody
+    @Secured("ROLE_ADMIN")
+    public User get(@PathVariable final String email ) {
+        return userRepository.findById(email).get();
+    }
+}
--- src/main/java/com/redi/demo/repository/UserRepository.java
+++ src/main/java/com/redi/demo/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.redi.demo.repository.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, String> {
+    User findByPassword(final String password);
+    User findByEmailOrderByName(final String email);
+    deleteByPassword()
 }
