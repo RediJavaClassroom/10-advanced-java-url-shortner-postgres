ed6a805b4f4e92bb2f46426552990fdb8501daa9
Kim Covaci <kimk@spotify.com>
2022-04-25 21:17:54 +0200

    Added expires at

--- src/main/java/com/redi/demo/repository/model/ShortLinkEntity.java
+++ src/main/java/com/redi/demo/repository/model/ShortLinkEntity.java
@@ -1,5 +1,6 @@
 package com.redi.demo.repository.model;
 
+import java.time.Instant;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -14,14 +15,17 @@ public class ShortLinkEntity {
   @Id
   private String key;
   private String originalUrl;
+  private Instant expiresAt;
+
   @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
   private User user;
 
   protected ShortLinkEntity() {}
 
-  public ShortLinkEntity(final String key, final String originalUrl) {
+  public ShortLinkEntity(final String key, final String originalUrl, final Instant expiresAt) {
     this.key = key;
     this.originalUrl = originalUrl;
+    this.expiresAt = expiresAt;
   }
 
   public String getKey() {
